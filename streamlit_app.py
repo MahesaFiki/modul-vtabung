7fc9dd73af6a89083173ee0bb9a5f279d92a8877
MahesaFiki <mahesafiki12@gmail.com>
2024-11-26 16:28:25 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,4 @@ r = st.number_input("Masukkan Jari-Jari :",0)
 t = st.number_input("Masukkan Tinggi :",0)
 if st.button("volume",type="primary")
   v = math.pi*(r**2)*t
-  st.success(f"Volume Tabung Adalah {v.2f} 
+  st.success(f"Volume Tabung Adalah {v:.2f}") 
