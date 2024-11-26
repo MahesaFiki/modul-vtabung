3c1461c690a730dc0e0814a7321793c911ebd7db
MahesaFiki <mahesafiki12@gmail.com>
2024-11-26 16:30:38 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import math 
 
-st.title(" Menghitung Volume Tabung :rainbow[Let's Go!] :rocket:")
+st.title(" Menghitung Volume Tabung :rainbow[Let's Go!] :hat:")
 r = st.number_input("Masukkan Jari-Jari :",0)
 t = st.number_input("Masukkan Tinggi :",0)
 if st.button("volume",type="primary"):
